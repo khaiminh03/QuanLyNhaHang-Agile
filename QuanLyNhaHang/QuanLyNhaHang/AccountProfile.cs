b926a379988cdab27fe02007a3a9a1f1bbba6dce
Phước Vinh <vinhne2603@gmail.com>
2024-05-11 13:36:04 +0700

    Thiết kế giao diên thông tin tài khoản

---
+++ QuanLyNhaHang/QuanLyNhaHang/AccountProfile.cs
@@ -0,0 +1,55 @@
+﻿using QuanLyNhaHang.DAO;
+using QuanLyNhaHang.DTO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyNhaHang
+{
+    public partial class AccountProfile : Form
+    {
+        private Account loginAccount;
+
+        public Account LoginAccount
+        {
+            get { return loginAccount; }
+            set { loginAccount = value;}
+        }
+
+        public AccountProfile(Account acc)
+        {
+            InitializeComponent();
+            LoginAccount = acc;
+        }
+
+        private void bntExitt_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            
+        }
+        public class AccountEvent:EventArgs
+        {
+            private Account acc;
+
+            public Account Acc 
+            { 
+                get { return acc; } 
+                set {  acc = value; }
+            }
+            public AccountEvent(Account acc)
+            {
+                this.Acc = acc;
+            }
+        }
+    }
+}
